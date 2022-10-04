5a6bcecd362e44852327089bde664f4a2f170b11
IrinaYareshko <92693031+IrinaYareshko@users.noreply.github.com>
2022-10-04 16:17:11 +0300

    Update ass1.m

--- Assignment_1/ass1.m
+++ Assignment_1/ass1.m
@@ -3,4 +3,4 @@ set(0,'defaulttextInterpreter','latex');
 set(groot,'defaultAxesTickLabelInterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
 
-%% 1) 
+%% 1) Luca is the most handsome guy i have ever met

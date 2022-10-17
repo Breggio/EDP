4fce364546e10b61714e62ed0fb7e59f89584f9f
Breggio <lucabreggi@gmail.com>
2022-10-17 19:25:28 +0300

    N

---
+++ Assignment_7/tracking.m
@@ -0,0 +1,69 @@
+function [x, z, x_Kalman] = tracking(N, T, sigma2_a, sigma2_n, Prob)
+
+eta = randn*sqrt(sigma2_n); % Random noise of measurments
+
+x(1) = 5;
+V(1) = 1;
+Z(1) = x(1) + eta; %the first measurment
+
+%State matrixes
+phi = [1 T; 0 1];
+G  = [T/2; T];    % input matrix
+H  = [1 0];       % observation matrix
+
+% Kalman filter 
+X = zeros(2, 2, N + 1); %true data
+P = zeros(2, 4, N + 1); %filtration error covariance matrix
+K = zeros(2, N);
+X_f = zeros(2, N);
+
+% Initial conditions
+X(:,:,1) = [2, 0; 0, 0];
+Q = G*G.'*sigma2_a; 
+R = sigma2_n;
+m = 7; % steps ahead
+
+for i=2:N
+
+    a = randn*sqrt(sigma2_a); %normally distributed random acceleration
+    eta = randn*sqrt(sigma2_n); %random noise of measurments
+    V(i) = V(i - 1) + a*T;
+    x(i) = x(i - 1) + V(i - 1)*T + a*T^2/2;
+
+    csi = rand;
+
+    if csi <= Prob
+        z(i) = NaN;
+        X(:,:,i) = X(:,:,i-1);
+        P(:,:,i) = P(:,:,i-1);
+        X_f = phi^m*X(:,1,i);
+    
+    elseif csi > Prob
+        z(i) = x(i) + eta;
+        X_f(:,i-1) = phi^7*X(:,1,i-1);
+        X(:,2,i-1) = phi*X(:,1,i-1);
+        P(:,3:4,i-1) = phi*P(:,1:2,i-1)*phi.'+Q;
+        K(:,i-1) = P(:,3:4,i-1)*H.'*(H*P(:,3:4,i-1)*H.'+R) ^(-1);
+
+        if isnan(z(i-1)) == 1 
+            counter=i ;
+            znew=z(i-1);
+            
+            while isnan(znew) == 1 
+                znew=z ( counter-1); 
+                counter=counter-1;
+            end
+        elseif isnan(z(i-1))== 0 
+            znew=z(i-1);
+        end
+        
+        X(:,1,i) = X(:,2,i-1)+K(:,i-1)*(znew-H*X(:,2, i-1) ) ;
+        P(:,1:2,i) = (eye(2)-K(:,i-1)*H)*P(:,3:4,i-1);
+
+    end
+end
+
+x_Kalman(1:N) = X(1,1,1:N);
+
+end
+
